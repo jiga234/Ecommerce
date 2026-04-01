fb48a4d43a39806d4d668adce011013ed8e95e8a
jiga234 <jigarprajapati2582005@gmail.com>
2026-04-01 17:46:02 +0530

    depoly website

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import ProductBar from '../Components/ProductBar';
 import Hero from '../Components/Home/Hero';
 import DeatilsBar from '../Components/Home/DeatilsBar';
 import CategoryBar from '../Components/Home/CategoryBar';
-import Offer from '../Components/Home/offer';
+import Offer from '../Components/Home/Offer';
 import DealBar from '../Components/Home/DealBar';
 import DiscontBar from '../Components/Home/DiscountBar';
 import Newsletter from '../Components/Home/Newsletter';
